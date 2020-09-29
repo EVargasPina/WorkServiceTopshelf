2560fea8e9c9fa331267b83bb66c068e06fefc04
Enzo Vargas <61596694+EVargasPina@users.noreply.github.com>
2020-09-29 13:38:44 -0500

    added admin execuion and check if workservice is runnig

--- ConsoleTopShelf/ConsoleTopShelf/LoggingService.cs
+++ ConsoleTopShelf/ConsoleTopShelf/LoggingService.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
 using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.ServiceProcess;
 using System.Text;
 using Topshelf;
 
@@ -16,9 +21,72 @@ namespace ConsoleTopShelf
             File.AppendAllText(_logFileLocation, DateTime.UtcNow.ToString() + " : " + logMessage + Environment.NewLine);
         }
 
+
         public bool Start(HostControl hostControl)
         {
-            Log("Starting");
+            //Revisa si esta instalado el workservice
+            ServiceController ctl = ServiceController.GetServices().FirstOrDefault(s => s.ServiceName == "TestService");
+            if (ctl == null)
+            {
+                //Proceso para ejecutar consola como cmd
+                Process process = new Process();
+                process.StartInfo.FileName = "cmd.exe";
+                process.StartInfo.CreateNoWindow = true;
+                process.StartInfo.RedirectStandardInput = true;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.Verb = "runas";
+                process.Start();
+                //Vamos a la ruta
+                process.StandardInput.WriteLine("cd " + @"C:\Users\ENZO\source\repos\WorkServiceTopshelf\ConsoleTopShelf\ConsoleTopShelf\bin\Release\netcoreapp3.1\win-x64\publish");
+                //Creamos
+                process.StandardInput.WriteLine("ConsoleTopShelf.exe install");
+                Console.WriteLine("Se creo workservice");
+                process.StandardInput.WriteLine("ConsoleTopShelf.exe start");
+                Console.WriteLine("Se inicio");
+                process.StandardInput.Flush();
+                process.StandardInput.Close();
+                Console.WriteLine(process.StandardOutput.ReadToEnd());
+
+                ServiceController sc = new ServiceController("TestService");
+                    
+                if(sc.Status == ServiceControllerStatus.Running)
+                {
+                    Log("Starting");
+                }
+
+            }
+            else
+            { 
+                //Revisa si el workservice se esta ejecutando
+                if (IsOS(OS_ANYSERVER) == false)
+                {
+                    //Proceso para ejecutar consola como cmd
+                    Process process = new Process();
+                    process.StartInfo.FileName = "cmd.exe";
+                    process.StartInfo.CreateNoWindow = true;
+                    process.StartInfo.RedirectStandardInput = true;
+                    process.StartInfo.RedirectStandardOutput = true;
+                    process.StartInfo.UseShellExecute = false;
+                    process.Start();
+                    //Vamos a la ruta
+                    process.StandardInput.WriteLine("cd " + @"C:\Users\ENZO\source\repos\WorkServiceTopshelf\ConsoleTopShelf\ConsoleTopShelf\bin\Release\netcoreapp3.1\win-x64\publish");
+                    //Creamos
+                    process.StandardInput.WriteLine("ConsoleTopShelf.exe start");
+                    Console.WriteLine("Se inicio");
+                    process.StandardInput.Flush();
+                    process.StandardInput.Close();
+                    Console.WriteLine(process.StandardOutput.ReadToEnd());
+
+                    ServiceController sc = new ServiceController("TestService");
+
+                    if (sc.Status == ServiceControllerStatus.Running)
+                    {
+                        Log("Starting");
+                    }
+                }
+            }
+
             return true;
         }
 
@@ -27,5 +95,10 @@ namespace ConsoleTopShelf
             Log("Stopping");
             return true;
         }
+
+        const int OS_ANYSERVER = 29;
+
+        [DllImport("shlwapi.dll", SetLastError = true, EntryPoint = "#437")]
+        public static extern bool IsOS(int os);
     }
 }
